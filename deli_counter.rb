c9f5253d65eed412a83ed2205aad678004394160
Tyler Firth <tyfirth@gmail.com>
2020-05-12 15:51:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 
 def now_serving(array)
   if array.empty?
-    puts "There is noboday waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array[0]}."
   end
